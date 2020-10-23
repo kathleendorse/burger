97a48ba5e5edffb7257e13866d4752564d672af7
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-10-23 15:40:36 -0500

    added route/js to change devoured status

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,26 +29,26 @@ router.post("/api/burgers", function(req, res) {
 }); 
 
 // //IN ACTIVITY CONDITION WAS SLEEPY/AWAKE- WE'RE TRYING TO USE THIS FOR DEVOURED
-// router.put("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
+ router.put("/api/burgers/:id", function(req, res) {
+     var condition = "id = " + req.params.id;
   
-//     console.log("condition", condition);
+     console.log("condition", condition);
   
-//     burger.update(
-//       {
-//         devoured: true
-//       },
-//       condition,
-//       function(result) {
-//         if (result.changedRows === 0) {
-//           // If no rows were changed, then the ID must not exist, so 404
-//           return res.status(404).end();
-//         }
-//         res.status(200).end();
+    burger.update(
+      {
+        devoured: true
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
   
-//       }
-//     );
-// });  
+      }
+    );
+});  
 
 
 
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,6 +3,7 @@ $(function(){
     //FUCKIN WORKS!
     $(".create-form").on("submit", function(e){
         e.preventDefault();
+
         var newburger = {
         name: $("#burg").val().trim(),
         devoured: 0
@@ -19,12 +20,26 @@ $(function(){
         );
     });
 
-    // $(".change-devoured").on("click", function(e){
-    //     e.preventDefault();
+    $(".change-devoured").on("click", function(e){
+        e.preventDefault();
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("newdevoured");
 
+        var newDevouredStatus = {
+            devoured: newDevoured
+        };
 
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredStatus
+        }).then(
+            function(){
+                console.log("this burger is now", newDevoured);
+                location.reload();
+            }
+        );
 
-    // });
+    });
 
 
 
