f952249bdbf2df27460661b31d275195f92f7d48
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-10-23 00:26:34 -0500

    css done, get all works, partials render

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,52 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    //burger comes from our model
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
 
-// 4. Create the `router` for the app,
+///MAY NEED TO ADDRESS REQ.BODY.NAME??  
+
+router.post("/api/burgers", function(req, res) {
+    burger.create(["burger_name", "devoured"], [req.body.name, false], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+}); 
+
+//IN ACTIVITY CONDITION WAS SLEEPY/AWAKE- WE'RE TRYING TO USE THIS FOR DEVOURED
+router.put("/api/cats/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update(
+      {
+        devoured: true
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+});  
+
+
+
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,36 @@
+//***COPIED FROM 16 MVC.
+
+
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 
 // * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
 
+var burger = {
+    all:  function(cb){
+        orm.all("burgers", function(res){
+            cb(res);
+        });
+    },
+      // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+
+
+
+
+
 // * Export at the end of the `burger.js` file.
 
-module.exports= burger
+module.exports= burger;
